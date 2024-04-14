6e6193418641a924e6ad800947b018f1f7c280f6
komboty <komubk@gmail.com>
2024-04-14 04:56:50 -0600

    ADD: Cadena de interfaz de usuario se pasa al juego

--- Assets/Scripts/NivelManager.cs
+++ Assets/Scripts/NivelManager.cs
@@ -13,11 +13,50 @@ public class NivelManager : MonoBehaviour
     public Constants constants;
     // Cadena a mover por las transiciones.
     public SplineAnimate stringMove;
+    // Contenedor de simbolos (cadena).
+    public Transform stringSymbols;
     // Transicion inicial.
     public SplineContainer transitionInitial;
+    // Cadena hecha por el usuario.
+    public Transform uiString;
+    // Prefabs del simbolo E.
+    public GameObject symbolE;
+    // Prefabs de cada simbolo del alfabeto del automata.
+    public List<GameObject> symbolsAlphabet;
 
     public void StartAutomata()
     {
+        // Auxiliar para la creacion de un simbolo.
+        GameObject symbolPrefa = null;
+        GameObject newSymbol = null;
+
+        for (int i = 0; i < uiString.childCount; i++) { 
+            Transform uiSymbol = uiString.GetChild(i);
+            // Se busca el modelo 3d del symbolo.
+            foreach (GameObject symbol in symbolsAlphabet)
+            {
+                if (symbol.name.Equals(uiSymbol.name))
+                {
+                    symbolPrefa = symbol;
+                }
+            }
+            // Se crea el symbolo.
+            newSymbol = Instantiate(symbolPrefa);
+            newSymbol.name = uiSymbol.name;
+            newSymbol.transform.SetParent(stringSymbols);
+            newSymbol.transform.position = new Vector3(stringSymbols.position.x,
+                stringSymbols.position.y, stringSymbols.position.z - (i * 1.2f));
+        }
+
+        // Se crea el symbolo E.
+        newSymbol = Instantiate(symbolE);
+        newSymbol.name = constants.SymbolEpsilonName;
+        newSymbol.transform.SetParent(stringSymbols);
+        newSymbol.transform.position = new Vector3(stringSymbols.position.x,
+            stringSymbols.position.y, 
+            stringSymbols.position.z - (uiString.childCount * 1.2f));
+
+        // Se inicia la animacion del automata. 
         stringMove.Container = transitionInitial;
         stringMove.Restart(true);
         CameraController.instance.followTransform = stringMove.transform;
